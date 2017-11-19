050af1f767bbc34a5e3b2d662ca9cb61d8217ca3
Yu Hao <jinyu121@126.com>
2017-11-19 09:56:56 +0800

    Use another dataset to test

--- evaluate.py
+++ evaluate.py
@@ -8,7 +8,7 @@ import argparse
 import os
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-from reader import ImageFolderDataset
+from reader import ImageTXTDataset
 from config import get_config, parse_config
 from model import get_model
 
@@ -30,11 +30,16 @@ def main(_):
         return image
 
     with tf.variable_scope("dataset"):
-        dataset = ImageFolderDataset(conf.data_train_dir,
-                                     batch_size=1,
-                                     pre_process_fn=pre_process_fn,
-                                     one_hot=False,
-                                     shuffle=conf.eval_random)
+        tmp_cls = sorted([f for f in os.listdir(conf.data_train_dir)
+                          if os.path.isdir(os.path.join(conf.data_train_dir, f))])
+        class_map = {name: ith for ith, name in enumerate(tmp_cls)}
+        dataset = ImageTXTDataset(os.path.join(conf.data_test_dir, 'images'),
+                                  os.path.join(conf.data_test_dir, 'val_annotations.txt'),
+                                  class_map=class_map,
+                                  batch_size=1,
+                                  pre_process_fn=pre_process_fn,
+                                  one_hot=False,
+                                  shuffle=conf.eval_random)
         image, label = dataset.get_data()
         label = tf.cast(label, tf.int64)
 
